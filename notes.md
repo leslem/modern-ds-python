d15c10833537ab59d51d50b218422fb090d7077a
Leslie Emery <leslie.s.emery@gmail.com>
2026-09-14 15:31:00 -0500

    Note on Polars through the afternoon break

--- notes.md
+++ notes.md
@@ -14,4 +14,35 @@
 - Also in R and JS, but python is the most mature
 - "Blazingly fast"
 - Pretty good API (though still not as good as tidyverse)
-- 
+- 50% of the API is expressions
+- with_columns vs. select
+    - `select` gives you only the columns you specify
+    - `with_columns` will give you all existing columns by default
+- Jeroen doesn't like `with_columns` because it's not a very and doesn't match the rest of the API, but now it's too late
+    - `with_columns` comes from Spark
+- **Expressions** don't do anything themselves (they are lazy)
+    - They are basically a tree describing how to construct the result (one or more series)
+    - The methods are what actually get something done
+- Expressions: aggregation, array, binary, categories, columns/names, computation, extension types, functions, list, manipulation/selection, meta, name, operators, string, struct, temporal, window
+- `pandas` does not have expressions -- everything is evaluated eagerly, not lazily
+- Q: can you generalize a set of data wrangling steps with polars expressions? e.g. remove_nas(colname="myColname")
+    - Yes, but write a python function that returns an expression
+- Expressions
+    - Lazy
+    - Function and data dependent
+    - Reusable
+    - Expressive
+    - Efficient
+    - Idiomatic
+- "Without pandas, we wouldn't have polars"
+- If you're working with other packages that require `pandas` dataframes as the input (e.g. require an index), you can just use `pl_df.to_pandas()`
+    - The key here is to do as much as you can in polars first before converting the end result to pandas
+- `plotnine` does work on polars dataframes, but only because it converts the df to a `pandas` df immediately behind the scenes
+- The default way of using polars is the "eager" API, but there's also a lazy API with `.lazy()`
+- The various `read_*()` functions have `scan_*()` equivalents that give you a lazy polars df instead of an eager one
+- There are some cases where having the entire data workflow as a lazy operation will be slower
+- Polars is not able to determine when (if you have multiples of an operation) it can be reused
+    - There's an example of this in the book (in ch1 ETL showcase) where polars does the same join twice
+
+## Great Tables
+
--- scratch.py
+++ scratch.py
@@ -1,3 +1,4 @@
+from polars import scan_csv
 import polars as pl
 
 from plotnine import *
@@ -25,3 +26,104 @@ ggplot(mpg, aes("displ", "hwy")) +\
     geom_point(aes(colour="class")) +\
     geom_smooth(se=False, method="lm") +\
     scale_color_brewer(palette='Set1')
+
+fruit = pl.read_csv("data/fruit.csv")
+fruit
+fruit.select(
+    pl.col('name'),
+    pl.col('^.*or.*$'),
+    pl.col('weight') / 1000,
+    'is_round'
+)
+# The is_round column was AI-generated and doesn't seem to be accurate
+
+fruit.with_columns(
+    pl.lit(True).alias('is_fruit'),
+    is_berry=pl.col('name').str.ends_with('berry')
+)
+
+fruit.filter(
+    (pl.col('weight') > 1000)
+     & pl.col('is_round')
+)
+
+fruit.group_by(pl.col('origin').str.split(' ').list.last()).agg(
+    pl.len(),
+    average_weight=pl.col('weight').mean()
+)
+
+fruit.sort(
+    pl.col('name').str.len_bytes(),
+    descending=True
+)
+
+origin_list = pl.col('origin').str.split(' ').list
+fruit.select(
+    first=origin_list.first(),
+    last=origin_list.last()
+)
+# in polars you CANNOT refer back to new columns you've created earlier in the same expression
+# because they are evaluated in parallel
+
+# Save an expression and reuse it later
+is_orange = (pl.col('color') == 'orange').alias('is_orange')
+fruit.with_columns(is_orange)
+
+# Eager vs. lazy API for a dataframe
+penguins = pl.read_csv("data/penguins.csv")
+penguins
+summary = penguins.group_by("species", "sex").agg(
+    pl.col('body_mass_g').mean().alias('avg_mass_g'),
+    pl.col('body_mass_g').count().alias('count')
+)
+summary
+summary.head(5)
+
+penguins = pl.scan_csv("data/penguins.csv")
+penguins
+penguins.show_graph()
+summary = penguins.group_by("species", "sex").agg(
+    pl.col('body_mass_g').mean().alias('avg_mass_g'),
+    pl.col('body_mass_g').count().alias('count')
+)
+summary
+summary.show_graph()
+summary.collect()
+summary.head(5)
+summary.head(5).collect()
+summary.profile()
+summary.explain()
+
+
+
+
+
+
+
+
+# Work through the cheat sheet with penguins
+penguins = pl.read_csv('data/penguins.csv', null_values="NA")
+penguins.filter(
+    pl.col('island') == 'Dream'
+)
+penguins.with_row_count()
+# It already has a row id in the csv
+penguins.with_row_index('rowid')  # this adds another column, rather than letting you set an existing column
+
+penguins.schema
+penguins.dtypes
+penguins.glimpse()
+penguins.describe()
+penguins.estimated_size()
+penguins.select(pl.col('rowid').cast(pl.UInt64))
+penguins.select(pl.col('rowid').cast(pl.Int8, strict=False))
+
+penguins.select('species', 'bill_depth_mm')
+penguins.select(pl.col('body_mass_g') * 0.03527396) # convert to oz
+penguins.select(body_mass_oz = pl.col('body_mass_g') * 0.03527396) # convert to oz
+penguins.select(pl.col('^bill')) # without a full match, regex select doesn't work
+penguins.select(pl.col('^bill.*$')) 
+
+import polars.selectors as cs
+
+
